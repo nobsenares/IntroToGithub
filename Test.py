4b32def141d505096a5b786196c626f51107e75f
Noah Obsenares <nobsenares@gmail.com>
2023-08-24 16:30:59 +0000

    1stProject

--- Test.py
+++ Test.py
@@ -1,1 +1,5 @@
-print("hello")
+print("Welcome to the Space Travel Calculator!")
+distance = int(input("Enter distance to the celestial object (in light years): "))
+speed = int(input("enter the spacecraft speed (in fraction of the speed of light): "))
+time = (distance/speed)
+print("It will take", time, "light-years to reach your dsetination")
